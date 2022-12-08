22d63858fa48c951d65503d31f6937aa619b929b
Diogo <diogoslva0133@gmail.com>
2022-12-08 09:25:01 -0300

    ESNext: Review

---
+++ Exercicios-Js/revisao1.js
@@ -0,0 +1,22 @@
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+console.log(a)
+
+//Template String 
+const produto = "ipad"
+console.log(`${produto} é caro!`)
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [x, y] = [1, 2, 3]
+console.log(x, y)
+
+
+//Desestruturando atributos de um obg, usa-se { } 
+const { idade: i, nome} = {nome: 'Ana', idade: 9}
+console.log(i, nome)
